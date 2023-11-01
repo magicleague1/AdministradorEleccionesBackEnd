94da58a5d4b2864d24f4eae484a7b94fc4220648
LuisFernandoCadimaCampos <nando1296@gmail.com>
2023-10-31 20:13:01 -0400

    Edicion y eliminación de frentes políticos

--- app/Http/Controllers/FrenteController.php
+++ app/Http/Controllers/FrenteController.php
@@ -11,7 +11,7 @@ class FrenteController extends Controller
 {
     public function index()
     {
-        $frentes = Frente::all();
+        $frentes = Frente::where('ARCHIVADO', false)->get();
 
         return response()->json($frentes);
     }
@@ -23,7 +23,8 @@ class FrenteController extends Controller
         $frente -> NOMBRE_FRENTE = $request->NOMBRE_FRENTE;
         $frente -> SIGLA_FRENTE = $request->SIGLA_FRENTE;
         $frente -> FECHA_INSCRIPCION = Carbon::now()->toDateString();
-        
+        $frente -> ARCHIVADO = false;
+
         $frente -> save();
 
         return response()->json(['message' => 'Se ha inscrito el frente correctamente.']);
@@ -31,14 +32,43 @@ class FrenteController extends Controller
 
     public function show($id)
     {
+        $frente = Frente::where('ARCHIVADO',false)->find($id);
+
+        if(!$frente)
+        {
+            return response()->json(['error' => 'No se encontró el frente.', 404]);
+        }
+
+        return response()->json($frente);
+    }
+
+    public function update(ValidarFrente $request, $id)
+    {
         $frente = Frente::find($id);
 
         if(!$frente)
         {
-            return response()->json(['error' => 'El frente no se encontró.', 404]);
+            return response()->json(['error' => 'No se encontró el frente.']);
         }
 
-        return response()->json($frente);
+        $frente -> NOMBRE_FRENTE = $request->NOMBRE_FRENTE;
+        $frente -> SIGLA_FRENTE = $request->SIGLA_FRENTE;
+        
+        $frente -> save();
+    }
+
+    public function destroy($id)
+    {
+        $frente = Frente::find($id);
+
+        if(!$frente)
+        {
+            return response()->json(['error' => 'No se encontró el frente.']);
+        }
+
+        $frente -> ARCHIVADO = true;
+
+        return response()->json(['message' => 'El frente se ha eliminado correctamente.']);
     }
 }
 
